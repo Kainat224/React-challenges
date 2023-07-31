e65b7c1d54594883af7361148676a5164bac3054
Kainat <anmolkainat244@gmail.com>
2023-07-31 09:22:20 -0700

    flash card update

---
+++ src/FlashCard.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const FlashCard = () => {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  const questions = [
+    {
+      id: 3457,
+      question: "What language is React based on?",
+      answer: "JavaScript",
+    },
+    {
+      id: 7336,
+      question: "What are the building blocks of React apps?",
+      answer: "Components",
+    },
+    {
+      id: 8832,
+      question:
+        "What's the name of the syntax we use to describe a UI in React?",
+      answer: "JSX",
+    },
+    {
+      id: 1297,
+      question: "How to pass data from parent to child components?",
+      answer: "Props",
+    },
+    {
+      id: 9103,
+      question: "How to give components memory?",
+      answer: "useState hook",
+    },
+    {
+      id: 2002,
+      question:
+        "What do we call an input element that is completely synchronised with state?",
+      answer: "Controlled element",
+    },
+  ];
+
+  return (
+    <div className="flashcards">
+      {questions.map((ques) => (
+        <div
+          key={questions.id}
+          onClick={() => handleClick(ques.id)}
+          className={ques.id === selectedId ? "selected" : ""}
+        >
+          <p>{ques.id === selectedId ? ques.answer : ques.question}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FlashCard;
